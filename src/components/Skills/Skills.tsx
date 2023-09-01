77bc1fa67f83d168f3bfb92062e4c85ce693e02f
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-09-01 13:32:46 +0300

    add skills and style for it

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,14 +2,21 @@ import { useContext } from "react";
 import { AppContext } from "../../App";
 
 export const Skills = () => {
-      const { userInfo } = useContext(AppContext);
+  const { userInfo } = useContext(AppContext);
 
-    return userInfo && (
-        <section id={userInfo.navigation[1][1]}>
-            <h2 className="hidden">Skills</h2>
-            <ul className="skills-list">
-                {userInfo.skills.map(el => (<li className="skills-box"><p className="skills-text">{el}</p></li>))}
-            </ul>
-        </section>
+  return (
+    userInfo && (
+      <section id={userInfo.navigation[1][1]}>
+        <h2 className="hidden">Skills</h2>
+        <ul className="skills-list">
+          {userInfo.skills.map((el) => (
+              <li className="skills-row" key={el[0]+'1'}>
+                  {el.map(skill=><p className="skills-box" key={skill}>{skill}</p>)}
+              
+            </li>
+          ))}
+        </ul>
+      </section>
     )
-}
+  );
+};
